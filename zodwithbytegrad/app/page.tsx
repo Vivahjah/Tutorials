ddf36accb482d874aadd0888606aa78785d6b19a
Mengwa Ugo <mengwa44@yahoo.com>
2025-04-09 01:25:55 +0100

    added form with Zod

--- zodwithbytegrad/app/page.tsx
+++ zodwithbytegrad/app/page.tsx
@@ -1,11 +1,14 @@
-import FormWithReactHookForm from "@/components/form-with-react-hook-form";
+// import FormWithReactHookForm from "@/components/form-with-react-hook-form";
 // import FormWithoutReactHookForm from "@/components/form-without-react-hook-form";
+
+import FormWithReactHookFormAndZod from "@/components/form-with-rhf-and-zod";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen bg-amber-50 text-gray-800 flex-col items-center justify-between p-24">
       {/* <FormWithoutReactHookForm />       */}
-      <FormWithReactHookForm />
+      {/* <FormWithReactHookForm /> */}
+      <FormWithReactHookFormAndZod />
     </main>
   );
 }
